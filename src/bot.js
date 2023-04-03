1223a391d39585d56da68cb37ed3a1707a8f12f5
Norbaev <timnrbv@gmail.com>
2023-04-03 16:45:17 +0500

    use ta.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Binance = require('binance-api-node').default;
+const ta = require('ta.js');
 
 const binanceApiKey =
   'Mwyek91lg0az1UUzh5W8ql2FZTtoDgQXqKei12yVt3AvKnASRMWbDenGle6cCXCj';
@@ -10,7 +11,7 @@ const telegramChatId = '521450044';
 let pairsToMonitor = [];
 
 const intervalToMonitor = '1d';
-const period = 12;
+const period = 20;
 
 const bot = new TelegramBot(telegramBotToken);
 
@@ -19,88 +20,41 @@ const client = Binance({
   apiSecret: binanceApiSecret,
 });
 
-function calculateSMA(closePrices, smaPeriod) {
-  const sma =
-    closePrices.slice(0, smaPeriod).reduce((sum, price) => sum + price, 0) /
-    smaPeriod;
-  return sma;
-}
-
-function calculateEMA(closePrices, emaPeriod) {
-  const k = 2 / (emaPeriod + 1);
-  let ema =
-    closePrices.slice(0, emaPeriod).reduce((sum, val) => sum + val, 0) /
-    emaPeriod;
-  for (let i = emaPeriod; i < closePrices.length; i++) {
-    ema = (closePrices[i] - ema) * k + ema;
-  }
-  return ema;
-}
-
-function calculateRSI(closePrices, rsiPeriod) {
-  let averageGain = 0;
-  let averageLoss = 0;
-
-  for (let i = 1; i <= rsiPeriod; i++) {
-    const diff = closePrices[i] - closePrices[i - 1];
-    if (diff > 0) {
-      averageGain += diff;
-    } else {
-      averageLoss += Math.abs(diff);
-    }
-  }
-  averageGain /= rsiPeriod;
-  averageLoss /= rsiPeriod;
-
-  for (let i = rsiPeriod + 1; i < closePrices.length; i++) {
-    const diff = closePrices[i] - closePrices[i - 1];
-    if (diff > 0) {
-      averageGain = (averageGain * (rsiPeriod - 1) + diff) / rsiPeriod;
-      averageLoss = (averageLoss * (rsiPeriod - 1)) / rsiPeriod;
-    } else {
-      averageGain = (averageGain * (rsiPeriod - 1)) / rsiPeriod;
-      averageLoss =
-        (averageLoss * (rsiPeriod - 1) + Math.abs(diff)) / rsiPeriod;
-    }
-  }
-
-  if (averageLoss === 0) {
-    return 100;
-  } else {
-    const RS = averageGain / averageLoss;
-    const RSI = 100 - 100 / (1 + RS);
-    return RSI;
-  }
-}
-
-function calculateWMA(closePrices, wmaPeriod) {
-  let weightedSum = 0;
-  let weightSum = 0;
-  for (let i = 0; i < wmaPeriod; i++) {
-    weightedSum += closePrices[i] * (wmaPeriod - i);
-    weightSum += wmaPeriod - i;
-  }
-  return weightedSum / weightSum;
-}
-
 async function checkPriceChanges() {
   let message = '';
   for (const pair of pairsToMonitor) {
     const candles = await client.candles({
       symbol: pair,
       interval: intervalToMonitor,
-      limit: period + 1,
+      limit: period,
     });
 
     const closePrices = candles.map((candle) => parseFloat(candle.close));
-    const sma = calculateSMA(closePrices, period);
-    const ema = calculateEMA(closePrices, period);
-    const wma = calculateWMA(closePrices, period);
-    const rsi = calculateRSI(closePrices, period);
-    const currentPrice = closePrices[period];
-    // const percentDifferenceFromSMA = ((currentPrice - sma) / sma) * 100;
-    // const percentDifferenceFromEMA = ((currentPrice - ema) / ema) * 100;
-    // const percentDifferenceFromWMA = ((currentPrice - wma) / wma) * 100;
+    const volumes = candles.map((candle) => parseFloat(candle.quoteVolume));
+    const currentPrice = closePrices[period - 1];
+    const openPrice = candles.map((candle) => parseFloat(candle.open));
+    const highPrice = candles.map((candle) => parseFloat(candle.high));
+    const lowPrice = candles.map((candle) => parseFloat(candle.low));
+
+    const sma = ta.sma(closePrices, period)[0];
+    const ema = ta.ema(closePrices, period)[0];
+    const wma = ta.wma(closePrices, period)[0];
+    // const macd = ta.macd(closePrices, 12, 24)[0];
+    // const rsi = ta.rsi(closePrices, period)[0];
+    const vwma = ta.vwma(
+      closePrices.map((price, index) => [price, volumes[index]]),
+      period
+    )[0];
+
+    const bop = ta.bop(
+      openPrice.map((openPrice, index) => [
+        openPrice,
+        highPrice[index],
+        lowPrice[index],
+        closePrices[index],
+      ]),
+      period
+    )[0];
 
     const percentDifferenceFromSMA =
       ((sma - currentPrice) / currentPrice) * 100;
@@ -108,30 +62,31 @@ async function checkPriceChanges() {
       ((ema - currentPrice) / currentPrice) * 100;
     const percentDifferenceFromWMA =
       ((wma - currentPrice) / currentPrice) * 100;
+    const percentDifferenceFromVWMA =
+      ((vwma - currentPrice) / currentPrice) * 100;
 
-    let priceStatus;
-    if (sma > currentPrice) {
-      priceStatus = '📈';
-    } else {
-      priceStatus = '📉';
-    }
-    if (
-      percentDifferenceFromSMA < -20 &&
-      percentDifferenceFromEMA < -20 &&
-      percentDifferenceFromWMA < -20
-    ) {
+    if (percentDifferenceFromSMA < -10) {
       message += `${pair}:\n`;
-      message += `- Текущая цена: ${currentPrice.toFixed(2)}\n`;
-      message += `- SMA ${sma.toFixed(
-        2
-      )} ${priceStatus} ${percentDifferenceFromSMA.toFixed(2)}%\n`;
-      message += `- EMA ${ema.toFixed(
-        2
-      )} ${priceStatus} ${percentDifferenceFromEMA.toFixed(2)}%\n`;
-      message += `- WMA ${wma.toFixed(
-        2
-      )} ${priceStatus} ${percentDifferenceFromWMA.toFixed(2)}%\n`;
-      message += `- RSI ${rsi.toFixed(2)}%\n\n`;
+      message +=
+        [
+          `- Текущая цена: ${currentPrice.toFixed(2)}`,
+
+          `- SMA ${sma.toFixed(2)} / ${percentDifferenceFromSMA.toFixed(2)}%`,
+
+          `- EMA ${ema.toFixed(2)} / ${percentDifferenceFromEMA.toFixed(2)}%`,
+
+          `- WMA ${wma.toFixed(2)} / ${percentDifferenceFromWMA.toFixed(2)}%`,
+
+          `- VWMA ${vwma.toFixed(2)} / ${percentDifferenceFromVWMA.toFixed(
+            2
+          )}%`,
+
+          // `- RSI ${rsi.toFixed(2)}%`,
+
+          // `- MACD ${macd.toFixed(2)}`,
+
+          `- Balance Of Power ${bop.toFixed(2)}`,
+        ].join('\n') + '\n\n';
     }
   }
 
@@ -156,8 +111,8 @@ async function getTopPairs() {
       return { pair: `${baseAsset}${quoteAsset}`, volume };
     })
     .sort((a, b) => b.volume - a.volume);
-  const top50Pairs = pairsByVolume.slice(0, 150).map((pair) => pair.pair);
-  return top50Pairs;
+  const topPairs = pairsByVolume.slice(0, 50).map((pair) => pair.pair);
+  return topPairs;
 }
 
 // setInterval(async () => {
