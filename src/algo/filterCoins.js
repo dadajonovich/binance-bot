221b9367a6db1956b95c58f41c87807245ca6a20
Norbaev <timnrbv@gmail.com>
2023-05-06 23:00:21 +0500

    left one branch

--- src/algo/filterCoins.js
+++ src/algo/filterCoins.js
@@ -3,7 +3,7 @@ const filterCoins = (coins, percentageDiffernce) => {
     (coin) =>
       coin.MACD.at(-1) > coin.MACD.at(-2) &&
       coin.RSI.at(-1) > coin.RSI.at(-2) &&
-      percentageDiffernce(coin.currentPrice, coin.VWMA.at(-1)) > 0.03 &&
+      percentageDiffernce(coin.currentPrice, coin.VWMA.at(-1)) > 0.05 &&
       coin.OBV.at(-1) > coin.OBV.at(-2)
   );
   return filteredCoins;
